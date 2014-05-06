0615caa1cba12e3abb1ac1ab2403698991a50f18
wmfo-ops <ops@wmfo.org>
2014-05-06 15:42:43 -0400

    Explicate credentials file

--- credentials.h
+++ credentials.h
@@ -1,6 +1,14 @@
 #define MYSQLPP_MYSQL_HEADERS_BURIED
 #include <mysql++/mysql++.h>
 
-//calls conn.connect(credentials);
 void setup_connection(mysqlpp::Connection& conn);
 
+/* credentials.cpp
+
+#include "credentials.h"
+
+void setup_connection(mysqlpp::Connection& conn){
+        conn.connect("Rivendell", "I.P.Add.ress", "user", "password");
+}
+
+*/
